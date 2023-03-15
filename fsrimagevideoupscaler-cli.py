42f4b9f4e52b7d0b201d00f86c669362958dcbe9
janis <jahu04@outlook.com>
2023-03-15 12:36:10 +0100

    fix quotes

--- fsrimagevideoupscaler-cli.py
+++ fsrimagevideoupscaler-cli.py
@@ -10,15 +10,15 @@
 import argparse
 import bin.handler
 
-ap = argparse.ArgumentParser(description="FSRImageVideoUpscaler - CLI")
-ap.add_argument("inputfile", help="File path for the video / image to be upscaled")
-ap.add_argument("outputfile", help="File path for the video / image that was upscaled")
-ap.add_argument('-s', '--scalefactor', help="Scale factor for the video / image")
+ap = argparse.ArgumentParser( description='FSRImageVideoUpscaler - CLI' )
+ap.add_argument( 'inputfile', help='File path for the video / image to be upscaled' )
+ap.add_argument( 'outputfile', help='File path for the video / image that was upscaled' )
+ap.add_argument( '-s', '--scalefactor', help='Scale factor for the video / image' )
 args = ap.parse_args()
 
 handler = bin.handler.Handler()
 
-if ( args.scalefactor[ len(args.scalefactor) -1:] == 'x' ):
+if ( args.scalefactor[ len(args.scalefactor) -1: ] == 'x' ):
     handler.handler( 'bin/lib/FidelityFX_CLI.exe', args.inputfile, 'custom', args.scalefactor, args.outputfile )
 else:
-    raise NameError( 'Argument Scale does require to be of form 2x!' )
+    raise NameError( 'Argument Scale does require to be of form 2x! (it has to end in x)' )
