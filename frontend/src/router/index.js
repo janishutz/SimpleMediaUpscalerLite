5e65f3b52c0fef8a4eb1cbaaeca37466c35c3007
janis <jahu04@outlook.com>
2023-05-24 10:45:26 +0200

    finish setup of electron app using vue & router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
@@ -18,8 +18,10 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: process.env.IS_ELECTRON ? createWebHashHistory() : createWebHistory(process.env.BASE_URL),
   routes
 })
+
+console.log( process.env.IS_ELECTRON );
 
 export default router
