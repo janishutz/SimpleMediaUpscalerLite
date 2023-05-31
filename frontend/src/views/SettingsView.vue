9eee7a9878a02078caaca25e6808fb6c78fbad6f
janis <jahu04@outlook.com>
2023-05-31 14:18:51 +0200

    add engine loading (technically)

--- frontend/src/views/SettingsView.vue
+++ frontend/src/views/SettingsView.vue
@@ -2,14 +2,24 @@
     <div class="about">
         <h1>Settings</h1>
         <h3>Engines</h3>
+        <p>{{ engines }}</p>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            engines: '',
+        }
+    },
     methods: {
         run() {
-            // run_script( 'ls' )
+            fetch( 'http://127.0.0.1:8081/api/getEngines' ).then( res => {
+                res.json().then( data => {
+                    this.engines = data.body;
+                } );
+            } );
         }
     }
 }
