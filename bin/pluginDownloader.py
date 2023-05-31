58e114e3683dfe03b83a91bf666df631a6b9b410
janis <jahu04@outlook.com>
2023-05-31 14:27:17 +0200

    begin implementation of plugin downloader

---
+++ bin/pluginDownloader.py
@@ -0,0 +1,9 @@
+"""
+*				ImageVideoUpscaler - pluginDownloader.py
+*
+*	Created by Janis Hutz 05/31/2023, Licensed under the GPL V3 License
+*			https://janishutz.com, development@janishutz.com
+*
+*
+"""
+
