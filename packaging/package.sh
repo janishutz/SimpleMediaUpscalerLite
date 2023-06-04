430c271f62a56f6bbc0977bc6feccde5837b5d58
Janis Hutz <development@janishutz.com>
2023-06-04 11:04:15 +0200

    add build script

--- packaging/package.sh
+++ packaging/package.sh
@@ -12,7 +12,9 @@ cp ./LICENSE ./dist/imagevideupscaler-linux
 cp ./logo.png ./dist/imagevideupscaler-linux
 
 # package rpm & deb
-# TODO: create packager
+cp ./dist/imagevideoupscaler-linux/* ./frontend/src/
+cd frontend
+npm run electron:build -- --linux deb rpm --win nsis
 
 # Make windows executable
 # TODO: create compiler
