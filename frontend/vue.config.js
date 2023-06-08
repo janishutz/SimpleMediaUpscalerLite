600fbc8cc7793ddc9439802c8d20585a2c832a77
Janis Hutz <development@janishutz.com>
2023-06-08 09:01:02 +0200

    fix packaging info

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
+      "appId": "com.janishutz.smuL",
+      "copyright": "Copyright (c) 2023 SimpleMediaUpscalerLite contributors",
+      "buildVersion": "V2.0.0-dev2",
       builderOptions: {
         files: [
           "**/*",
