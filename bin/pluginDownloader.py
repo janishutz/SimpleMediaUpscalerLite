cecb01fba4aaea356156c88439f09c2cafe62e63
ThatPlasma <dartgaming17@gmail.com>
2024-01-10 18:33:48 +1100

    Removed a feck ton

--- bin/pluginDownloader.py
+++
@@ -1,9 +0,0 @@
-"""
-*				SimpleMediaUpscalerLite - pluginDownloader.py
-*
-*	Created by Janis Hutz 05/31/2023, Licensed under the GPL V3 License
-*			https://janishutz.com, development@janishutz.com
-*
-*
-"""
-
