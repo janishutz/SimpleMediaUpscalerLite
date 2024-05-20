a8eb1a60f5e8ed8ba22781a8a84c9ec64d186dcc
Janis Hutz <development@janishutz.com>
2024-05-20 07:28:48 +0200

    restart from scratch

--- packaging/prepareWindowsPackaging.sh
+++
@@ -1,7 +0,0 @@
-curl -o ./pythonInstaller.exe https://www.python.org/ftp/python/3.11.3/python-3.11.3-amd64.exe
-
-wine pythonInstaller.exe
-
-wine python -m pip install pyinstaller
-
-printf '\n\n==> Done installing python for windows\n\n'
